9052d6c7753ebd5ab369e1c46cca5f717866044a
uzair762 <ua0378309@gmail.com>
2025-11-05 10:30:04 +0500

    Add files via upload
     Ask user for pizza size (small, medium, large) → validate using while(true) until correct. 
    ▪ Assign base price: small=100, medium=200, large=300. 
    ▪ Ask if user wants pepperoni: small=+30, medium/large=+50. 
    ▪ Ask if user wants extra cheese: +20. 
    ▪ Use continue for invalid inputs, break when valid. 
    ▪ Show final bill.

---
+++ Pizzabillingsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pizzabillingsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public class Pizzabillingsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
